68f4b6ece79b117db0ca9ddfaf5d6c5675491d4d
moolsan <github@lotus-continuum.de>
2012-02-08 18:29:43 +0100

    more stuff

---
+++ manifests/dbserver.pp
@@ -0,0 +1,28 @@
+group { 'puppet': ensure => 'present' }
+
+class mysql_5 {
+  
+  package { "mysql-server-5.1":
+    ensure => present
+  }
+  
+  service { "mysql":
+    ensure => running, 
+    require => Package["mysql-server-5.1"]
+  }
+
+  exec { "create-db-schema-and-user":
+    command => "/usr/bin/mysql -uroot -p -e \"drop database if exists testapp; create database testapp; create user dbuser@'%' identified by 'dbuser'; grant all on testapp.* to dbuser@'%'; flush privileges;\"",
+    require => Service["mysql"]
+  }
+
+  file { "/etc/mysql/my.cnf":
+    owner => 'root',
+    notify => Service['mysql'],
+    source => '/vagrant/files/my.cnf'
+  }
+
+}
+
+include mysql_5
+
